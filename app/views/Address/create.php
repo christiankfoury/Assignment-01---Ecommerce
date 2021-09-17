75a1e5fe4d0c48d11b05657af542a5633e8a7175
Christian Kfoury <christian_kfoury558@hotmail.ca>
2021-09-17 19:49:19 -0400

    Made text fields for adding an address

--- app/views/Address/create.php
+++ app/views/Address/create.php
@@ -2,13 +2,18 @@
 <head><title>Add a Vaccine</title></head><body>
 
 <?php $this->view('Main/details',$data); //call the animal details view ?>
-
-Adding a vaccine for  
-<?php echo $data->colour, " ", $data->species; ?>
+<br><br>
+Adding an address for  
+<?php echo $data-> person_id, ", ", $data->last_name, ", ", $data->first_name; ?>
 
 <form action='' method='post'>
-	Vaccine type: <input type='text' name='type' /><br>
-	Vaccine date: <input type='date' name='date' /><br>
+	Description: <input type='text' name='description' /><br>
+	Street address: <input type='text' name='street_address' /><br>
+	City: <input type='text' name='city' /><br>
+	Province: <input type='text' name='province' /><br>
+	Zip code: <input type='text' name='zip_code' /><br>
+	Country_code: <input type='text' name='country_code' /><br>
+	  <!-- <select name="country_code" id="country_code"> -->
 	<input type='submit' name='action' value='Create' />
 </form>
 
